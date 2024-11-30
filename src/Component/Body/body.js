f8fdadda54235b7ff1837fab3b119f3b8e70dd9a
Mansi Singh <66991274+mansisingh2803@users.noreply.github.com>
2024-12-01 00:39:48 +0530

    CORS commit

--- src/Component/Body/body.js
+++ src/Component/Body/body.js
@@ -22,7 +22,7 @@ const Body = () => {
     );
 
     async function getRestaurants() {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         console.log(json);
 
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -14,7 +14,7 @@ const useRestaurantMenu = (resId) => {
     const fetchData = async () => {
         let encoded_id = encodeURIComponent(resId);
         //console.log(encoded_id);
-        let data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4594965&lng=77.0266383&restaurantId=" + encoded_id);
+        let data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4594965&lng=77.0266383&restaurantId=" + encoded_id);
         let json = await data.json();
 
         setResInfo(json.data);
